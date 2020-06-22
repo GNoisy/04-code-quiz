3886ba3b1bdc8bdbc3769b03050632bd7b88e7f9
Giovanni Noisy <gnoisy77@gmail.com>
2020-06-22 07:37:22 -0700

    added object to store questions and answers

---
+++ assets/script.js
@@ -0,0 +1,59 @@
+//present user start button if they want to start quiz
+
+//if user clicks start - start timer of game clock
+
+//create list of questions for the user to answer
+var questions = [
+    {
+        question: "JavaScript currently has this many number of variable declarations?",
+        choices: {
+            a: "One",
+            b: "Two",
+            c: "Three",
+            d: "four",
+        },
+        answer: "Two",
+    },
+    {
+        question: "If you put two commas in a row in an array literal, the value will eaqual what?",
+        choices: {
+            a: "True",
+            b: "NaN",
+            c: "Null",
+            d: "Undefined",
+        },
+        answer: "Null", 
+    },
+    {
+        question: "A for loop repeats until a specific condition evaluates to what?",
+        choices: {
+            a: "True",
+            b: "False",
+            c: "Undefined",
+            d: "Return",
+        },
+        answer: "True",
+    },
+    {
+        question: "A string is enclosed what type of punctuation?",
+        choices: {
+            a: "Period",
+            b: "Quotation-mark",
+            c: "Brackets",
+            d: "Semicolon",
+        },
+        answer: "Quotation-mark",
+    },
+    {
+        question: "The Modulus arithmetic operator is written with which symbol?",
+        choices: {
+            a: "/",
+            b: "*",
+            c: "%",
+            d: "()",
+        },
+        answer: "%",
+    },
+
+];
+
